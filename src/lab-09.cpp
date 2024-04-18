8c7c08473168ee21b3d2c96ed30d0e322c51b9dc
TonyMontana00 <130142221+TonyMontana00@users.noreply.github.com>
2024-04-18 11:04:08 +0200

    Update lab-09.cpp

--- src/lab-09.cpp
+++ src/lab-09.cpp
@@ -10,7 +10,7 @@ main(int argc, char *argv[])
   const unsigned int degree_velocity = 2;
   const unsigned int degree_pressure = 1;
 
-  const double T      = 1.0;
+  const double T      = 0.2;
   const double deltat = 0.05;
   const double theta  = 1.0;
 
